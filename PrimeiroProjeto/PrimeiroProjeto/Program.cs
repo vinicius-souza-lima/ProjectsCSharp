1776bcf9183263d5417aff4a57b4d411fd5c023f
Vinícius de Souza Lima <eng.viniciusdesouzalima@gmail.com>
2024-09-04 22:22:15 -0300

    Primeiro Projeto

---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    internal class Program{
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
