0afffb514be16345fad98f0d9a02a839490d3301
Vinícius de Souza Lima <eng.viniciusdesouzalima@gmail.com>
2024-09-06 22:45:01 -0300

    Remove ignored files

---
+++ Exercícios 1/Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("SOMA = " + (n1 + n2));
+        }
+    }
+}
---
+++ Exercícios 1/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double x = Program.Circunferencia(raio);
+            Console.WriteLine(x,"F2",CultureInfo.InvariantCulture);
+        }
+
+        static public double Circunferencia(double raio) {
+            double pi = 3.14159;
+            return pi * raio * raio;
+        }
+    }
+}
