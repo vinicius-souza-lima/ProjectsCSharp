b07c7bcb4f3f5b210776a3ecac4d8430115b2d88
Vinícius de Souza Lima <eng.viniciusdesouzalima@gmail.com>
2024-09-07 12:16:22 -0300

    Exercicio 5 concluido

---
+++ Exercícios 1/Exercicio5/Exercicio5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            string[] vec1 = Console.ReadLine().Split(" ");
+            string[] vec2 = Console.ReadLine().Split(" ");
+
+            Console.WriteLine("VALOR A PAGAR: R$ " + (float.Parse(vec1[1],CultureInfo.InvariantCulture) *
+                                                      float.Parse(vec1[2],CultureInfo.InvariantCulture) +
+                                                      float.Parse(vec2[1],CultureInfo.InvariantCulture) *
+                                                      float.Parse(vec2[2],CultureInfo.InvariantCulture)).ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
