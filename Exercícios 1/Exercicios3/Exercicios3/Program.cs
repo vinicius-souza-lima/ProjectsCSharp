fb394299930325263420d3a3b75798dc3fa11f16
Vinícius de Souza Lima <eng.viniciusdesouzalima@gmail.com>
2024-09-07 00:15:20 -0300

    Exercicio 1.3 Resolvido

---
+++ Exercícios 1/Exercicios3/Exercicios3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            int D = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIFERENCA = "+ (A * B - C * D));
+        }
+    }
+}
