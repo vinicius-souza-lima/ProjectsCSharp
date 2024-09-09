c4c06f32319be5ae8146747123527d8407329180
Vinícius de Souza Lima <eng.viniciusdesouzalima@gmail.com>
2024-09-08 22:29:34 -0300

    Exercicio 4 Grupo 2 Seção 3

---
+++ Exercícios Seção 3/Grupo 2/Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            string[] input = Console.ReadLine().Split(" ");
+            int hora1 = int.Parse(input[0]);
+            int hora2 = int.Parse(input[1]);
+
+            int duracao = 24 - hora1 + hora2;
+
+            if (duracao > 24)
+                duracao -= 24;
+
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORAS(S)");
+        }
+    }
+}
